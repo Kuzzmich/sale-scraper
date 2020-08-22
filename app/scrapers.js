694ea1ea0858f82ab6f2d5071a239f0f15d0dc01
Alexey Kramin <kramin.alexey@gmail.com>
2020-08-22 18:18:47 +0300

    - fixed asos scraper

--- app/scrapers.js
+++ app/scrapers.js
@@ -7,20 +7,8 @@ const telegramMessageQueue = require('./queues/telegramQueue');
 
 
 const asos = async (sendNotification) => {
-  /*const parsedProductsList = await parsers.asosFetch();
-  const insertedProducts = await services.syncProductsCollection(parsedProductsList);*/
-  const insertedProducts = [
-    {
-      "url": "https://www.asos.com/ru/nike/cherno-belye-krossovki-nike-renew-lucent/prd/21412462?clr=chernyj-belyj&colourwayid=60164838&SearchQuery=&cid=1935",
-      "discount": 1,
-      "img": "https://images.asos-media.com/products/cherno-belye-krossovki-nike-renew-lucent/21412462-1-blackwhite?$n_480w$&wid=476&fit=constrain",
-      "name": "Черно-белые кроссовки Nike Renew Lucent",
-      "newPrice": 5590,
-      "oldPrice": 5690,
-      "shop": "asos",
-      "timestamp": 1597856447886.0
-    }
-  ]
+  const parsedProductsList = await parsers.asosFetch();
+  const insertedProducts = await services.syncProductsCollection(parsedProductsList);
 
   if (sendNotification && insertedProducts.length) {
     console.log('sending telegram notification');
